8d7d2e987cc14f621ee7c7aec8632d72ee0f1fac
gt-asup6vm <basket.baza@gmail.com>
2017-10-14 16:54:25 +0300

    Подготовка проекта для Ильича

--- TestCurl.php
+++ TestCurl.php
@@ -1,36 +1,58 @@
 <?php
 
+function exportToFile ($fileName, $context)
+{
+    if (is_writable($fileName)) {
+        if (!$handle = fopen($fileName, 'a')) {
+            echo "Ошибка открытия файла ($fileName)!";
+            exit();
+        }
+        if (fwrite($handle, $context) === FALSE) {
+            echo "Ошибка записи содержимого в файл ($fileName)";
+            exit();
+        }
+        echo "Содержимое записано в файл ($fileName)";
+        fclose($handle);
+    } else {
+        echo "Файл $fileName не доступен для записи";
+    }
+}
+
 
 $ch = curl_init();
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // возвращает результат в переменную а не в буфер
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true); //использовать редиректы
-    curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36'); //выставляем настройки браузера
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // работа с https
-    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false); // работа с https
-    curl_setopt($ch, CURLOPT_URL, "https://5element.by/ajax/catalog_category_list.php?SECTION_ID=157");
-    curl_setopt($ch, CURLOPT_POST, 1);
-$a=curl_exec($ch);
-$s=json_decode($a);
-//var_dump($s);
-curl_close($ch);
 
-$ch = curl_init("https://5element.by/ajax/catalog_category_list.php?SECTION_ID=157");
-$categoryId=$s->updateSection->section->UF_IB_RELATED_ID;
+if ($_SERVER['COMPUTERNAME'] == 'GT-ASUP6VM') {
+    curl_setopt($ch, CURLOPT_PROXY, "172.16.15.33");
+    curl_setopt($ch, CURLOPT_PROXYPORT, 3128);
+    curl_setopt($ch, CURLOPT_PROXYUSERPWD, "gt-asup6:teksab");
+}
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // возвращает результат в переменную а не в буфер
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true); //использовать редиректы
 curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36'); //выставляем настройки браузера
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // работа с https
 curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false); // работа с https
-curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_URL, "https://5element.by/ajax/catalog_category_list.php?SECTION_ID=157");
-curl_setopt($ch, CURLOPT_POSTFIELDS,
-        "categoryId=.'$categoryId'.&currentPage=1&itemsPerPage=150&viewType=1&sortName=popular&sortDest=desc&searchQuery=&fastFilterId=&filterInStock=1&filterInStore=0");
-// выводим инфу из секции JSON
+curl_setopt($ch, CURLOPT_POST, 1);
+$a = curl_exec($ch);
+$s = json_decode($a);
 
+$categoryId = $s->updateSection->section->UF_IB_RELATED_ID;
 
-var_dump(curl_exec($ch));
+$postField = "categoryId=" . $categoryId . "&currentPage=1&itemsPerPage=150&viewType=1&sortName=popular&sortDest=desc&searchQuery=&fastFilterId=&filterInStock=1&filterInStore=0";
+curl_setopt($ch, CURLOPT_POSTFIELDS, $postField);
+
+$a = curl_exec($ch);
+$stripa=stripslashes($a);
+//exportToFile('export.html',$stripa);
+
+$s = json_decode($a);
+
+//exportToFile('export1.html',$s);
+echo $a."\r\n";
+
+echo $stripa;
 /*
-//var_dump($s->updateSection);
+/
 echo $s->updateSection->section->ID."\n\r";
 echo $s->updateSection->section->UF_IB_RELATED_ID."\n\r";
 echo $s->updateSection->section->NAME."\n\r";
--- index.php
+++ index.php
@@ -5,3 +5,10 @@
  * Date: 12.10.2017
  * Time: 12:45
  */
+$s='<li href="ksdj">';
+$sl=addslashes($s);
+$ss=stripcslashes($sl);
+
+echo  $s."\r\n";
+echo  $sl."\r\n";
+echo $ss."\r\n";;
